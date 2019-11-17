b1b98cec594c9fd62d0f62dc89374190ff1c3fd1
Rob <robcolanoleary@hotmail.com>
2019-11-17 19:58:34 +0000

    First Edit

--- HelloPython.py
+++ HelloPython.py
@@ -1,5 +1,5 @@
 import datetime
-print("Hello world!")
+print("Group 2 Assignment")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
