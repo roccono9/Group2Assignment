529d55528c0f79dd40d2a4b512a540202766a029
Rob <robcolanoleary@hotmail.com>
2019-11-19 20:03:01 +0000

    Python Ready

--- HelloPython.py
+++ HelloPython.py
@@ -1,10 +1,8 @@
 import datetime
-//echo "Group 2 Assignment"
 print("Group 2 Assignment")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
-Jenkins ALL=(ALL) NOPASSWD:ALL
 
 
 	
